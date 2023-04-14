6df36762edd776e0114e44f7e4c7cd38906da51f
hoangtuincepit <128338344+hoangtuincepit@users.noreply.github.com>
2023-04-14 10:04:51 +0700

    1.1.0

---
+++ Tasks/TaskPurchasing.cs
@@ -0,0 +1,44 @@
+using Tasks;
+
+namespace Purchasing.Tasks
+{
+    public class TaskPurchasing
+    {
+        private readonly Awaiter<string> _awaiter = new();
+        private IPurchasing _purchasing;
+        private readonly AwaiterTask<string> _task;
+
+        public TaskPurchasing()
+        {
+            _task = new AwaiterTask<string>(_awaiter);
+        }
+
+        private IPurchasing Purchasing
+        {
+            set
+            {
+                _purchasing = value;
+                value.Purchased += OnPurchase;
+            }
+        }
+
+        public void Clear()
+        {
+            _awaiter.Clear();
+            _purchasing.Purchased -= OnPurchase;
+            _purchasing = null;
+        }
+
+        private void OnPurchase(string message, bool success)
+        {
+            _awaiter.Complete(success ? null : message);
+        }
+
+        public ITask<string> PurchaseAsync(string productId)
+        {
+            _awaiter.Clear();
+            _purchasing.Purchase(productId);
+            return _task;
+        }
+    }
+}
